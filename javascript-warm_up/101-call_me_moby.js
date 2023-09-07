d74bdf92852e426a8ae2041e2459daf4f17b664b
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-09-07 12:57:08 +0100

    Call me Moby

---
+++ javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,11 @@
+// 101-call_me_moby.js
+
+// Define the callMeMoby function
+function callMeMoby(x, theFunction) {
+    for (let i = 0; i < x; i++) {
+      theFunction();
+    }
+  }
+  
+  // Export the callMeMoby function to make it visible from outside
+  module.exports.callMeMoby = callMeMoby;  
