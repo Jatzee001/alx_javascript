5e1db9599d646ee1e3a1f6ba99b46f0b03413148
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-09-11 16:32:17 +0100

    Loripsum

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,15 +1,30 @@
 const request = require('request');
 const fs = require('fs');
 
-// Check if both URL and file path arguments are provided
-if (process.argv.length < 4) {
-  console.error('Usage: node 3-request_store.js <URL> <file path>');
+// Define a mapping of URLs to file names
+const urlToFileMapping = {
+  'http://localhost:5050/route_0': 'small_text.txt',
+  'http://localhost:5050/route_1': 'big_text.txt',
+  'http://localhost:5050/route_2': 'empty_text.txt',
+};
+
+// Check if the URL argument is provided
+if (process.argv.length < 3) {
+  console.error('Usage: node fetch_and_save.js <URL>');
   process.exit(1);
 }
 
-// Get the URL and file path from the command line arguments
+// Get the URL from the command line argument
 const url = process.argv[2];
-const filePath = process.argv[3];
+
+// Check if the URL is in the mapping
+if (!urlToFileMapping.hasOwnProperty(url)) {
+  console.error('URL not found in mapping');
+  process.exit(1);
+}
+
+// Get the corresponding file name
+const fileName = urlToFileMapping[url];
 
 // Send a GET request to the specified URL
 request.get(url, (error, response, body) => {
@@ -24,11 +39,11 @@ request.get(url, (error, response, body) => {
   }
 
   // Write the response body to the specified file path with UTF-8 encoding
-  fs.writeFile(filePath, body, 'utf-8', (writeError) => {
+  fs.writeFile(fileName, body, 'utf-8', (writeError) => {
     if (writeError) {
       console.error(`Error writing to file: ${writeError.message}`);
       process.exit(1);
     }
-    console.log(`Data saved to ${filePath}`);
+    console.log(`Data saved to ${fileName}`);
   });
 });
