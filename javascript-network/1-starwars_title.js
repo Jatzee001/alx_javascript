9d253634e85e759790b0970ad110834b8222d90b
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-09-11 16:16:53 +0100

    Star wars movie title

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,21 +1,35 @@
 const request = require('request');
 
-// Check if the URL argument is provided
+// Check if the movie ID argument is provided
 if (process.argv.length < 3) {
-  console.error('Usage: node 0-statuscode.js <URL>');
+  console.error('Usage: node 1-starwars_title.js <movie ID>');
   process.exit(1);
 }
 
-// Get the URL from the command line argument
-const url = process.argv[2];
+// Get the movie ID from the command line argument
+const movieId = process.argv[2];
 
-// Send a GET request to the specified URL
-request.get(url, (error, response, body) => {
+// Construct the URL for the Star Wars API
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Send a GET request to the API
+request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 
-  // Display the status code
-  console.log(`code: ${response.statusCode}`);
+  if (response.statusCode !== 200) {
+    console.error(`Error: Failed to fetch data. Status code: ${response.statusCode}`);
+    process.exit(1);
+  }
+
+  try {
+    const movieData = JSON.parse(body);
+    const title = movieData.title;
+    console.log(title);
+  } catch (parseError) {
+    console.error('Error parsing API response:', parseError.message);
+    process.exit(1);
+  }
 });
